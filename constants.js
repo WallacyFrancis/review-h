115d525f6d08a9eecd0cebf4dd5278f6008d859a
Henrique <henriquezambon.s@gmail.com>
2024-04-06 14:50:01 -0300

    fix : constans.js

---
+++ constants.js
@@ -0,0 +1,27 @@
+import dayjs from 'dayjs'
+
+export const PATH_SYSTEM = 'http://localhost:3000/'
+
+export const API = 'https://gw.audiency.io'
+// export const API = 'https://homolog-api.audiency.io' 
+
+export const ADVERTISER_PLAYER = '/advertiser-rest/audioStream'
+
+export const APP = 'https://app.audiency.io'
+
+// export const ENDPOINT_ADVERTISER = '/client'
+
+export const ENDPOINT_ADVERTISER = '/advertiser'
+export const ENDPOINT_AUDIENCY = '/audiency'
+export const ENDPOINT_LOGIN = '/login'
+export const ENDPOINTREST = '/advertiser-rest'
+export const AUDIO_DOWNLOAD = '/client-rest/download-audio'
+export const STATION_PLAYER = '/client-rest/audioStream'
+export const PLAYER = 'client-rest/audioStream'
+// export const PLAYER = 'advertiser-rest/audioStream'
+
+export const IMAGEPATHS = 'https://gw.audiency.io/advertiser-rest/image?token='
+// export const IMAGEPATHS = 'https://homolog-api.audiency.io/advertiser-rest/image?token='
+
+export const VERSION_API = '2.0.0'
+export const YEAR_PARAMS = dayjs().format('YYYY')
